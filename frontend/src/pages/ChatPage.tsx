9479da08df9628a37142171325c6bbb6e1447d23
mnaggarse <mnaggarse@gmail.com>
2025-09-10 15:49:26 +0300

    Make basic chat page

---
+++ frontend/src/pages/ChatPage.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+export default function ChatPage() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [text, setText] = useState("");
+
+  const handleSend = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setMessages((prevMessages) => [...prevMessages, text]);
+    setText("");
+  };
+
+  return (
+    <div>
+      {/* Messages Container */}
+      <div className="w-full p-4 pb-24 space-y-2 h-screen bg-gray-50 overflow-y-scroll">
+        {messages.map((message, index) => (
+          <Card key={index} className="w-fit py-2 px-4">
+            <CardContent className="p-0">{message}</CardContent>
+          </Card>
+        ))}
+      </div>
+
+      {/* Send Form */}
+      <form
+        onSubmit={handleSend}
+        className="border-t bg-white w-full fixed bottom-0 left-0 p-4 flex justify-between items-center gap-2"
+      >
+        <Input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="py-5"
+          type="text"
+          placeholder="Enter a message..."
+        />
+        <Button className="font-semibold" size="lg">
+          Send
+        </Button>
+      </form>
+    </div>
+  );
+}
